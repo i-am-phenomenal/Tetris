a1f52c49fe084a003d604854b5dcedbffccb320e
Aditya Chaturvedi <chat29aditya@gmail.com>
2020-06-27 17:52:42 +0530

    WIP: block collision mechanism

--- engine.py
+++ engine.py
@@ -14,16 +14,15 @@ def load_image(image_name):
 
 pygame.init()
 screen_height = 600
-screen_width = 600
+screen_width = 575
 window = pygame.display.set_mode((screen_height, screen_width))
 clock = pygame.time.Clock()
 
 black_color = (0, 0, 0)
 white_color = (200, 200, 200)
-blue_color = (0, 0, 128)
+# blue_color = (0, 0, 128)
 images_dir = os.getcwd() + "/Images/"
 grid = load_image("grid.jpg")
-block_size = 25 #Set the size of the grid block
 cube_block = load_image("cube-block.png")
 i_block = load_image("i-block.png")
 j_block = load_image("j-block.png")
@@ -31,8 +30,19 @@ l_block = load_image("L-block.png")
 r_s_block = load_image("r-s-block.png")
 s_block = load_image("s-block.png")
 t_block = load_image("t-block.png")
+score_board = load_image("score_board.jpg")
 tetrominos = [cube_block, i_block, j_block, l_block, r_s_block, s_block, t_block]
+GRIDSIZE = screen_height // 24
+COLLAPSED_BLOCKS = []
 
+
+def render_collapsed_blocks():
+    global COLLAPSED_BLOCKS
+    if COLLAPSED_BLOCKS == []: 
+        return
+    else: 
+        for block_dict in COLLAPSED_BLOCKS: 
+            window.blit(block_dict["current_block"], (block_dict["x_coord"], block_dict["y_coord"]))
 
 def render_window_in_center(): 
     x_pos =  screen_height / 2 - screen_width / 2
@@ -40,28 +50,39 @@ def render_window_in_center():
     os.environ['SDL_VIDEO_WINDOW_POS'] = '%i,%i' % (x_pos, y_pos)
     os.environ['SDL_VIDEO_CENTERED'] = '0'
 
+
+# def render_block_after_stoppage(block_count, x_coord, y_coord): 
+#     current_block = tetrominos[block_count]
+#     window.blit(current_block, (x_coord, y_coord))
+
 def generate_tetrominos(x_coord, y_coord, counter): 
     if counter == 0: 
         block = tetrominos[0] 
     else: 
-        block = random.choice(tetrominos)  
+        block = tetrominos[counter]
         
     window.blit(block, (x_coord, y_coord))
 
 def render_grid():
-    for x in range(0, 20):
-        for y in range(int(screen_width / block_size)):
-            rect = pygame.Rect(x*block_size, y*block_size, block_size, block_size)
-            pygame.draw.rect(window, white_color, rect, 1) # to render the border of the rectangle 
+    """ Draw horizontal and vertical lines on the entire game window.
+        Space between the lines is GRIDSIZE.
+    """
+    for i in range(15):
+        pygame.draw.line(window, black_color, (i * GRIDSIZE, 0), (i * GRIDSIZE, screen_height), 1)
 
+    for i in range(24):
+        pygame.draw.line(window, black_color, (0, i * GRIDSIZE), (GRIDSIZE * 24, i * GRIDSIZE), 1)
+
+def render_score_board(): 
+    window.blit(score_board, (375, 0))
 
 def game_loop():
     global window, clock, grid
     render_window_in_center()
-    block_speed = 1
+    block_speed = 2
     block_start_y_coord = -50
     block_start_x_coord = random.randrange(0, 200)
-    block_count = 0
+    block_count = 0 
 
     while True:
         for event in pygame.event.get():
@@ -69,19 +90,34 @@ def game_loop():
                 pygame.quit()
                 sys.exit()
 
-            if event.type == pygame.KEYDOWN: 
-                if event.key == pygame.K_ESCAPE: 
-                    pygame.quit()
-                    quit()
+        if event.type == pygame.KEYDOWN: 
+            if event.key == pygame.K_ESCAPE: 
+                pygame.quit()
+                quit()
+
+            if event.key == pygame.K_DOWN: 
+                block_speed += 2
+
+        if event.type == pygame.KEYUP: 
+            if event.key == pygame.K_DOWN: 
+                block_speed = 2
 
         window.blit(grid, (0, 0))
         render_grid()
+        render_score_board()
+        render_collapsed_blocks()
         generate_tetrominos(block_start_x_coord, block_start_y_coord, block_count)
         block_start_y_coord += block_speed
-
-        if block_start_y_coord > screen_height: 
+        
+        if block_start_y_coord == 530:
+            global COLLAPSED_BLOCKS
+            block_dict = {"current_block": tetrominos[block_count], "x_coord": block_start_x_coord, "y_coord": block_start_y_coord} 
+            COLLAPSED_BLOCKS.append(block_dict)
+            # render_block_after_stoppage(block_count, block_start_x_coord, block_start_y_coord)
             block_count = random.randrange(0, 7)
-
+            block_start_y_coord = -50
+            block_start_x_coord = random.randrange(0, 200)
+            
 
         pygame.display.update()
         clock.tick(60)
